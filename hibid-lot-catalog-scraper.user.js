dc8587730671f63afe842067e6ac0bbc25c3d3a2
Admin <ashbycollado@gmail.com>
2026-07-09 10:21:12 -0400

    Force HiBid scraper content injection

--- hibid-lot-catalog-scraper.user.js
+++ hibid-lot-catalog-scraper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         HiBid Lot Catalog Scraper
 // @namespace    http://tampermonkey.net/
-// @version      1.4.1
+// @version      1.4.2
 // @description  Switches HiBid catalog pages to Single Page, expands live catalogs, scrolls lazy-loaded lots, and copies enriched lot/bid data to JSON.
 // @updateURL    https://raw.githubusercontent.com/AshbyCollado/hibid-userscripts/main/hibid-lot-catalog-scraper.user.js
 // @downloadURL  https://raw.githubusercontent.com/AshbyCollado/hibid-userscripts/main/hibid-lot-catalog-scraper.user.js
@@ -20,6 +20,7 @@
 // @include      https://hibid.com/*
 // @include      https://*.hibid.com/*
 // @run-at       document-idle
+// @inject-into  content
 // @noframes
 // @grant        GM_setClipboard
 // @grant        GM.setClipboard
